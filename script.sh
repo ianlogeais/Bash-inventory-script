f771f1808fcaf5608afaf61a01e0c061d0494d8e
ianlogeais <71567996+ianlogeais@users.noreply.github.com>
2021-10-19 21:58:09 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+rm -r chemin
+touch chemin
+function chemin
+{
+        echo -n "Veuillez saisir le nom du dossier à analyser : "
+        read path
+}
+function exploreR
+{
+    files=$(find $path)
+
+    for element in $files
+    do
+        if test -f "$element"
+        then
+            echo $element >> chemin
+        elif test -d "$path/$element"
+        then
+            echo $element >> chemin
+        elif test -d "$path/$element"
+            then
+                exploreR $element
+        fi
+
+    done
+}
+
+function complet
+{
+    chemin
+    exploreR $path
+}
+
+complet
+
+echo -e "\nNous avons trouvé $(cat chemin | wc -l)  fichiers"
+cat chemin
